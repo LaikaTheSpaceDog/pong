d784c673a1a884df49376bc3e434abc200a20801
Oscar Wales <oscarjwales@gmail.com>
2020-09-27 18:25:42 +0100

    Updates

---
+++ precache-manifest.7331b8fb86a6673d94b82eb321f2f1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62ca4274b94fc771d253a898f9d08c1",
+    "url": "/pong/index.html"
+  },
+  {
+    "revision": "8f2068c5b034dc79a42c",
+    "url": "/pong/static/css/main.dad60b71.chunk.css"
+  },
+  {
+    "revision": "c6ef2e9f5acf460100bc",
+    "url": "/pong/static/js/2.a6b8a0cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pong/static/js/2.a6b8a0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2068c5b034dc79a42c",
+    "url": "/pong/static/js/main.a7c5b224.chunk.js"
+  },
+  {
+    "revision": "b180fcfa987ff2cd231a",
+    "url": "/pong/static/js/runtime-main.319658e4.js"
+  }
+]);
