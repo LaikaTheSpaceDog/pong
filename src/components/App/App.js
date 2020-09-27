3f9010a99177e49307272ccf939bfed65856cb8a
Oscar Wales <oscarjwales@gmail.com>
2020-09-27 18:24:53 +0100

    Set repo up for deployment on github pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import "../../styles/css/styles.min.css"
 import Landing from "../Landing/Landing";
 import Home from "../Home";
